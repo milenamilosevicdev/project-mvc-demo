84ec3dba6873860cbf95b9993b88fa91eaa649c4
milenamilosevicdev <mmilosevicds@gmail.com>
2025-11-05 17:04:01 +0100

    new feature

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-function findMax(arr) {
-  return Math.max(...arr);
+function newFunction() {
+  console.log("New function");
 }
-
-function displayMaxValue(maxValue) {
-  let element = document.createElement("p");
-  element.classList.add("max-paragraph");
-  element.innerText = `Max element is ${maxValue}`;
-  maxDivEl.appendChild(element);
-}
-
-btn.addEventListener("click", function () {
-  displayMaxValue(findMax(array));
-});
